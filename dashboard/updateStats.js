9fb2cd309632230cdb08bbc1fb3013a37be19264
Kian Abdollahi <kabdollahi347@gmail.com>
2025-03-24 15:03:27 -0700

    change formatting

--- dashboard/updateStats.js
+++ dashboard/updateStats.js
@@ -26,8 +26,27 @@ const getLocaleDateStr = () => (new Date()).toLocaleString()
 const getStats = () => {
     document.getElementById("last-updated-value").innerText = getLocaleDateStr()
     
-    makeReq(PROCESSING_STATS_API_URL, (result) => updateCodeDiv(result, "processing-stats"))
-    makeReq(ANALYZER_API_URL.stats, (result) => updateCodeDiv(result, "analyzer-stats"))
+    makeReq(PROCESSING_STATS_API_URL, (result) => {
+        const output = `Heaviest Container: ${result.heaviest_container}\n` +
+                   `Lightest Container: ${result.lightest_container}\n` +
+                   `Last Updated: ${result.last_updated}\n` +
+                   `Max Containers Onboard: ${result.max_containers_onboard}\n` +
+                   `Containers Processed: ${result.num_containers_proccessed}\n` +
+                   `Ships Arrived: ${result.num_ships_arrived}`;
+
+        document.getElementById("processing-stats").innerText = output;
+    });
+
+
+
+
+    makeReq(ANALYZER_API_URL.stats, (result) => {
+        const output = `Container Events: ${result.num_container_events}\n` +
+                   `Ship Events: ${result.num_ship_events}`;
+                   
+        document.getElementById("analyzer-stats").innerText = output;
+
+    });
     // makeReq(ANALYZER_API_URL.ship, (result) => updateCodeDiv(result, "event-ship"))
     // makeReq(ANALYZER_API_URL.container, (result) => updateCodeDiv(result, "event-container"))
 
