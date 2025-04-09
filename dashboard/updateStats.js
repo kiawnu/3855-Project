6d8d7b1de5b13c78ebd14ef1d9a2451445a4292d
Kian Abdollahi <kabdollahi347@gmail.com>
2025-04-09 12:40:29 -0700

    fix urls

--- dashboard/updateStats.js
+++ dashboard/updateStats.js
@@ -1,10 +1,10 @@
 /* UPDATE THESE VALUES TO MATCH YOUR SETUP */
 
-const PROCESSING_STATS_API_URL = "http://microservice-3855.westus2.cloudapp.azure.com/processing/stats"
+const PROCESSING_STATS_API_URL = "/processing/stats"
 const ANALYZER_API_URL = {
-    stats: "http://microservice-3855.westus2.cloudapp.azure.com/analyzer/stats",
-    ship: "http://microservice-3855.westus2.cloudapp.azure.com/analyzer/ship_arrival",
-    container: "http://microservice-3855.westus2.cloudapp.azure.com/analyzer/container_processing"
+    stats: "/analyzer/stats",
+    ship: "/analyzer/ship_arrival",
+    container: "/analyzer/container_processing"
 }
 
 // This function fetches and updates the general statistics
@@ -62,8 +62,8 @@ const getStats = () => {
 
 //START
 document.addEventListener('DOMContentLoaded', () => {
-  const updateUrl = 'http://microservice-3855.westus2.cloudapp.azure.com/consistency_check/update';
-  const checksUrl = 'http://microservice-3855.westus2.cloudapp.azure.com/consistency_check/checks';
+  const updateUrl = '/consistency_check/update';
+  const checksUrl = '/consistency_check/checks';
 
   document.getElementById('run-checks-button').addEventListener('click', async () => {
     const shipEventCode = document.getElementById('update-result');
