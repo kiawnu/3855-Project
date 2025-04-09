b1b022862069bf84c856b76a583ee1166d11fe59
Kian Abdollahi <kabdollahi347@gmail.com>
2025-04-09 09:39:16 -0700

    fix bug in update endpoint

--- consistency_check/app.py
+++ consistency_check/app.py
@@ -34,39 +34,37 @@ storage_container_ids = app_config["endpoints"]["storage_stats"]["container_url"
 
 
 def run_consistency_checks():
+    # Start logger
     logger.info("Update process has started...")
 
+    # Time function
     start = time.time()
 
-    processing_event_counts_r = httpx.get(prcessing_stats)
+    # Query each endpoint, convert to json from http response
 
+    processing_event_counts_r = httpx.get(prcessing_stats)
     processing_event_counts = processing_event_counts_r.json()
 
     analyzer_ship_event_ids_r = httpx.get(analyzer_ship_ids)
-
     analyzer_ship_event_ids = analyzer_ship_event_ids_r.json()
 
     analyzer_container_event_ids_r = httpx.get(analyzer_container_ids)
-
     analyzer_container_event_ids = analyzer_container_event_ids_r.json()
 
     storage_ship_event_ids_r = httpx.get(storage_ship_ids)
-
     storage_ship_event_ids = storage_ship_event_ids_r.json()
 
     storage_container_event_ids_r = httpx.get(storage_container_ids)
-
     storage_container_event_ids = storage_container_event_ids_r.json()
 
     ship_analyzer_count = len(analyzer_ship_event_ids)
     ship_storage_count = len(storage_ship_event_ids)
+
     container_analyzer_count = len(analyzer_container_event_ids)
     container_storage_count = len(storage_container_event_ids)
 
     trace_ids_ship_queue = {event["trace_id"] for event in analyzer_ship_event_ids}
-
     trace_ids_ship_db = {event["trace_id"] for event in storage_ship_event_ids}
-
     trace_ids_container_queue = {
         event["trace_id"] for event in analyzer_container_event_ids
     }
@@ -81,29 +79,31 @@ def run_consistency_checks():
     missing_container_in_queue = trace_ids_container_db - trace_ids_container_queue
 
     missing_ship_events_in_db = [
-        event for event in trace_ids_ship_db if event["trace_id"] in missing_ship_in_db
+        event
+        for event in storage_ship_event_ids
+        if event["trace_id"] in missing_ship_in_db
     ]
 
     missing_ship_events_in_queue = [
         event
-        for event in trace_ids_ship_queue
+        for event in analyzer_ship_event_ids
         if event["trace_id"] in missing_ship_in_queue
     ]
 
     missing_container_events_in_db = [
         event
-        for event in trace_ids_container_db
+        for event in storage_container_event_ids
         if event["trace_id"] in missing_container_in_db
     ]
 
     missing_container_events_in_queue = [
         event
-        for event in trace_ids_container_queue
+        for event in analyzer_container_event_ids
         if event["trace_id"] in missing_container_in_queue
     ]
 
     end = time.time()
-    processing_time_ms = start - end
+    processing_time_ms = end - start
 
     current_time = datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%fZ")
 
